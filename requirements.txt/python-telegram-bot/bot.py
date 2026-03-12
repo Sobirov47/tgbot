db124b10f24aa1b844037cb875acf711239a97b7
Sobirov47 <akbarsobiroov496@gmail.com>
2026-03-12 23:51:05 +0500

    Add files via upload

---
+++ requirements.txt/python-telegram-bot/bot.py
@@ -0,0 +1,37 @@
+import yt_dlp
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+TOKEN = "8774297321:AAFBa1Y_dhyR-_OKtvmKwwAsB8gMGeOSvLU"
+
+async def download(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    url = update.message.text
+    await update.message.reply_text("⬇️ Скачиваю видео...")
+
+    try:
+        ydl_opts = {
+            'format': 'best',
+            'outtmpl': 'video.%(ext)s',
+            'noplaylist': True,
+        }
+
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            info = ydl.extract_info(url, download=True)
+            filename = ydl.prepare_filename(info)
+
+        with open(filename, "rb") as video:
+            await update.message.reply_video(video)
+
+        os.remove(filename)
+
+    except Exception as e:
+        await update.message.reply_text(f"❌ Ошибка: {e}")
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, download))
+
+print("Бот запущен...")
+app.run_polling()
